01ccd646d326ad6c0526d46741e99931efdc4a35
cyberbex <cyberbex@gmail.com>
2022-06-20 20:24:17 -0300

    banco funfando

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
-
+import 'package:sqflite/sqflite.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
 // ignore: import_of_legacy_library_into_null_safe
 import 'package:http/http.dart' as http;
 import 'dart:async';
@@ -31,8 +32,56 @@ class _HomeState extends State<Home> {
 
       postagens.add(p);
     }
-    print(postagens);
+
     return postagens;
+  }
+
+  _recuperarBancoDados() async {
+    final caminhoBancoDados = await getDatabasesPath();
+    final localBancoDados = join(caminhoBancoDados, "banco.db");
+
+    var bd = await openDatabase(localBancoDados, version: 1,
+        onCreate: (db, dbVersaoRecente) {
+      String sql =
+          "CREATE TABLE usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR, idade INTEGER) ";
+      db.execute(sql);
+    });
+
+    return bd;
+    //print("aberto: " + bd.isOpen.toString());
+  }
+
+  _salvar() async {
+    Database bd = await _recuperarBancoDados();
+
+    Map<String, dynamic> dadosUsuario = {"nome": "bruno", "idade": 23};
+    int id = await bd.insert("usuarios", dadosUsuario);
+    print("Salvo: $id ");
+  }
+
+  _listarUsuarios() async {
+    Database bd = await _recuperarBancoDados();
+
+    //String sql = "SELECT * FROM usuarios WHERE id = 5 ";
+    //String sql = "SELECT * FROM usuarios WHERE idade >= 30 AND idade <= 58";
+    //String sql = "SELECT * FROM usuarios WHERE idade BETWEEN 18 AND 46 ";
+    //String sql = "SELECT * FROM usuarios WHERE idade IN (18,30) ";
+    //String filtro = "an";
+    //String sql = "SELECT * FROM usuarios WHERE nome LIKE '%" + filtro + "%' ";
+    //String sql = "SELECT *, UPPER(nome) as nomeMaiu FROM usuarios WHERE 1=1 ORDER BY UPPER(nome) DESC ";//ASC, DESC
+    String sql = "SELECT * FROM usuarios"; //ASC, DESC
+    List usuarios = await bd.rawQuery(sql);
+
+    for (var usuario in usuarios) {
+      print("item id: " +
+          usuario['id'].toString() +
+          " nome: " +
+          usuario['nome'] +
+          " idade: " +
+          usuario['idade'].toString());
+    }
+
+    //print("usuarios: " + usuarios.toString() );
   }
 
   post() async {
@@ -51,6 +100,10 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
+    //_recuperarBancoDados();
+    //_listarUsuarios();
+    //_salvar();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Consumo de serviço avançado'),
